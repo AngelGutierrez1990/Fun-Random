8d7615422cb7a569f2822c864458ed1f6a8d8a20
AngelGutierrez1990 <32337207+AngelGutierrez1990@users.noreply.github.com>
2018-03-16 17:28:27 +0000

    First Commit

---
+++ Oscar Table Tennis.py
@@ -0,0 +1,14 @@
+print ( "Hi Oscar, how many times has Angel beaten you at Table Tennis?")
+amount=input()
+print ( 'Wow! That is so embarrasing ... You must really suck or was it a fluke?')
+amount=input()
+print ( 'What happened, why did you loose?')
+amount=input()
+print ( 'You know what, I will help you out, watch this & you might have a chance - Sound good?')
+amount=input()
+
+import webbrowser
+webbrowser.open("https://www.youtube.com/watch?v=ov_FO78q9lw")
+
+#
+open("link.html", "w").write('<a href="http://www.example.com"> Good luck </a>')
